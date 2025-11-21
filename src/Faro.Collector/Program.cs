e0b42dd31b3eec3030a2ffd23976acf6be6762ca
Sean Ankenbruck <sean.ankenbruck@gmail.com>
2025-11-21 13:21:03 -0500

    register services and build app

--- src/Faro.Collector/Program.cs
+++ src/Faro.Collector/Program.cs
@@ -1,41 +1,37 @@
+using FluentValidation;
+using Faro.Collector.Services;
+using Faro.Shared.Validation;
+using Faro.Storage;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add Faro services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add validation
+builder.Services.AddValidatorsFromAssemblyContaining<MetricPointValidator>();
+
+// Add storage layer
+builder.Services.AddMetricsStorage(builder.Configuration);
+
+// Add metrics buffer as hosted service
+builder.Services.AddHostedService<MetricsBuffer>();
+builder.Services.AddSingleton<MetricsBuffer>(sp =>
+    (MetricsBuffer)sp.GetServices<IHostedService>()
+        .First(s => s is MetricsBuffer));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
