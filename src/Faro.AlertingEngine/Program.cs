f7c000d3797a322befd1d496f6ecc3e0810e383f
Sean Ankenbruck <sean.ankenbruck@gmail.com>
2025-12-03 13:09:41 -0500

    register all alerting engine services

--- src/Faro.AlertingEngine/Program.cs
+++ src/Faro.AlertingEngine/Program.cs
@@ -1,8 +1,14 @@
 using Faro.AlertingEngine;
+using Faro.AlertingEngine.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Add Alerting Engine service
+// Register alerting services
+builder.Services.AddSingleton<IAlertRuleStore, FileBasedAlertRuleStore>();
+builder.Services.AddSingleton<IAlertStateManager, InMemoryAlertStateManager>();
+builder.Services.AddSingleton<IAlertEvaluator, AlertEvaluator>();
+
+// Register Alerting Engine worker
 builder.Services.AddHostedService<AlertingEngineWorker>();
 
 var host = builder.Build();
