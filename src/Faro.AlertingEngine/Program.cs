4bd7dec18a2736bd1720214195d4934e9a5b7384
Sean Ankenbruck <sean.ankenbruck@gmail.com>
2025-12-09 12:52:22 -0500

    register email and webhook notification channels in DI container

--- src/Faro.AlertingEngine/Program.cs
+++ src/Faro.AlertingEngine/Program.cs
@@ -1,5 +1,7 @@
 using Faro.AlertingEngine;
 using Faro.AlertingEngine.Services;
+using Faro.Notifications;
+using Faro.Notifications.Channels;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -7,6 +9,25 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddSingleton<IAlertRuleStore, FileBasedAlertRuleStore>();
 builder.Services.AddSingleton<IAlertStateManager, InMemoryAlertStateManager>();
 builder.Services.AddSingleton<IAlertEvaluator, AlertEvaluator>();
+
+// Register notification channels
+builder.Services.AddHttpClient();
+
+// Configure and register email notification channel
+builder.Services.Configure<EmailConfiguration>(
+    builder.Configuration.GetSection("Notifications:Channels:email"));
+builder.Services.AddSingleton<INotificationChannel, EmailNotificationChannel>();
+
+// Configure and register webhook notification channel
+var webhookUrl = builder.Configuration["Notifications:Channels:webhook:Url"];
+if (!string.IsNullOrEmpty(webhookUrl))
+{
+    builder.Services.AddSingleton<INotificationChannel>(sp =>
+        new WebhookNotificationChannel(
+            sp.GetRequiredService<IHttpClientFactory>(),
+            sp.GetRequiredService<ILogger<WebhookNotificationChannel>>(),
+            webhookUrl));
+}
 
 // Register Alerting Engine worker
 builder.Services.AddHostedService<AlertingEngineWorker>();
